57dfc9e80b5ec8c75c091a4513f0b012850222f4
Shaquille Mayers <69588483+shaquillemayers@users.noreply.github.com>
2025-04-14 19:18:48 +0100

    Add error handling Netlify fn

--- netlify/functions/getHero.js
+++ netlify/functions/getHero.js
@@ -2,14 +2,21 @@ exports.handler = async (event) => {
   const { heroName } = event.queryStringParameters;
   const API_KEY = process.env.SUPERHERO_API_KEY;
 
-  const response = await fetch(`https://www.superheroapi.com/api/${API_KEY}/search/${heroName}`);
-  const data = await response.json();
+  try {
+    const response = await fetch(`https://www.superheroapi.com/api/${API_KEY}/search/${heroName}`);
+    const data = await response.json();
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(data),
-  };
+    return {
+      statusCode: response.status,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Something went wrong on the server." }),
+    };
+  }
 };
